72a9a82c015968806629afc195dd462c56e9e837
Vidya Mani <mani.vidya@gmail.com>
2014-04-16 14:18:28 -0700

    Concatenating secret key by line breaks. Formatted last updated date.

--- check_project_activity.rb
+++ check_project_activity.rb
@@ -26,16 +26,15 @@ def get(url, access_key_id, secret_access_key)
   Nokogiri::XML(response.body)
 end
 
-if ARGV.length < 4
+if ARGV.length < 3
   p "USAGE: Please pass in the following arguments: "
   p "1. host                      - e.g. https://some.mingle.thoughtworks.com"
   p "2. access_key_id             - your username"
-  p "3. secret_access_key(part 1) - the HMAC key from your profile, the part before newline"
-  p "3. secret_access_key(part 2) - the HMAC key from your profile, the part after newline"
+  p "3. secret_access_key         - split the key into separate args by line breaks"
 else
   host = ARGV[0]
   access_key_id = ARGV[1]
-  secret_access_key = "#{ARGV[2]}\n#{ARGV[3]}"
+  secret_access_key = ARGV[2..-1].join("\n")
 
   hostname = URI.parse(host).hostname
 
@@ -45,7 +44,7 @@ else
 
 
   CSV.open("#{hostname}.csv", "wb+") do |csv|
-    csv << ["Project Identifier", "Last Updated At"]
+    csv << ["Project Identifier", "Last Updated On"]
     identifiers.each do |identifier|
 
       url = "#{host}/api/v2/projects/#{identifier}/feeds/events.xml"
@@ -53,9 +52,10 @@ else
 
       doc.remove_namespaces!
       last_updated_time = doc.xpath("//entry//updated").map(&:text).first
-      csv << [identifier, last_updated_time]
+      last_updated_date = last_updated_time ? Time.parse(last_updated_time).to_date.to_s : nil
+      csv << [identifier, last_updated_date]
 
-      puts "Project #{identifier} was last updated at #{last_updated_time.inspect}"
+      puts "Project #{identifier} was last updated on #{last_updated_date.inspect}"
     end
   end
 end
