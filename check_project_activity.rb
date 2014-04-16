678bf9c700b41dee5c07b928301ba89beebe14fd
Vidya Mani <mani.vidya@gmail.com>
2014-04-16 13:16:41 -0700

    Using HMAC and ApiAuth to authenticate. Hacked in blank function on string. Needs fixing.

--- check_project_activity.rb
+++ check_project_activity.rb
@@ -1,34 +1,61 @@
-require 'net/http'
+require "uri"
+require "net/https"
+require "time"
+require "api_auth"
 require 'nokogiri'
+require 'csv'
 
-def get(url, username, password)
-  uri = URI(url)
-  request = Net::HTTP::Get.new(uri) # => String
-  request.basic_auth username, password
-
-  response = Net::HTTP.start(uri.hostname, uri.port) do |http|
-    http.request(request)
+class String
+  def blank?
+    empty?
   end
+end
+
+
+def get(url, access_key_id, secret_access_key)
+
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+
+  request = Net::HTTP::Get.new(uri.request_uri)
+  ApiAuth.sign!(request, access_key_id, secret_access_key)
+
+  response = http.request(request)
 
   Nokogiri::XML(response.body)
 end
 
-host = ARGV[0]
-username = ARGV[1]
-password = ARGV[2]
+if ARGV.length < 4
+  p "USAGE: Please pass in the following arguments: "
+  p "1. host                      - e.g. https://some.mingle.thoughtworks.com"
+  p "2. access_key_id             - your username"
+  p "3. secret_access_key(part 1) - the HMAC key from your profile, the part before newline"
+  p "3. secret_access_key(part 2) - the HMAC key from your profile, the part after newline"
+else
+  host = ARGV[0]
+  access_key_id = ARGV[1]
+  secret_access_key = "#{ARGV[2]}\n#{ARGV[3]}"
 
-url = "#{host}/api/v2/projects.xml"
-doc = get(url, username, password)
-identifiers = doc.xpath("//identifier").map(&:text)
+  hostname = URI.parse(host).hostname
 
-identifiers.each do |identifier|
+  url = "#{host}/api/v2/projects.xml?name_and_id_only"
+  doc = get(url, access_key_id, secret_access_key)
+  identifiers = doc.xpath("//identifier").map(&:text)
 
-  url = "#{host}/api/v2/projects/#{identifier}/feeds/events.xml"
-  doc = get(url, username, password)
 
-  doc.remove_namespaces!
-  last_updated_time = doc.xpath("//entry//updated").map(&:text).first
+  CSV.open("#{hostname}.csv", "wb+") do |csv|
+    csv << ["Project Identifier", "Last Updated At"]
+    identifiers.each do |identifier|
 
-  puts "Project #{identifier} was last updated at #{last_updated_time.inspect}"
+      url = "#{host}/api/v2/projects/#{identifier}/feeds/events.xml"
+      doc = get(url, access_key_id, secret_access_key)
 
+      doc.remove_namespaces!
+      last_updated_time = doc.xpath("//entry//updated").map(&:text).first
+      csv << [identifier, last_updated_time]
+
+      puts "Project #{identifier} was last updated at #{last_updated_time.inspect}"
+    end
+  end
 end
